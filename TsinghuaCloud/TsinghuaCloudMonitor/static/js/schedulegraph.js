4ae223f5455357d80bc219b82be2419be95b6f51
pwwpcheng <pwwp@outlook.com>
2015-07-03 13:14:41 +0800

    Craking Crack Cracked~
    Qunee say goodbye~

--- TsinghuaCloud/TsinghuaCloudMonitor/static/js/schedulegraph.js
+++ TsinghuaCloud/TsinghuaCloudMonitor/static/js/schedulegraph.js
@@ -1,4 +1,19 @@
+var canvas = document.getElementById('canvas');
 var graph = new Q.Graph(canvas);
+
+action = function(){
+    $('#monitor-table').datagrid({
+        url:'scheduledatatable/'
+    });
+    console.info("123");
+};
+setInterval("action()", 3000);
+setInterval("tenants()", 3000);
+
+$(function () {
+    action();
+    tenants();
+});
 
 function createText(name, x, y, fontSize, color, parent) {
     var text = graph.createText(name, x, y);
@@ -14,24 +29,18 @@ function createText(name, x, y, fontSize, color, parent) {
 }
 
 graph_init = function() {
-    graph.destroy();
-    graph.styles = {};
-    graph.styles[Q.Styles.ARROW_TO_STYLES] = {
-        lineWidth: 1,
-        lineJoin: "round"//"miter"
-    };
-    graph = new Q.Graph(canvas);
-    createText("监控实例图", 0, -200, 20, "#F00");
-    createText("图例", 300, -200, 15, "#00");
-    var cloudmodel = graph.createNode("监控云", 300, -110);
+    graph.clear();
+    createText("监控实例图", -50, -200, 20, "#F00");
+    createText("图例", 250, -200, 15, "#00");
+    var cloudmodel = graph.createNode("监控云", 250, -110);
     cloudmodel.setStyle(Q.Styles.LABEL_POSITION, Q.Position.RIGHT_MIDDLE);
     cloudmodel.setStyle(Q.Styles.LABEL_OFFSET_X, 30);
     cloudmodel.image = "Q-cloud";
-    var servermodel = graph.createNode("主机", 300, -50);
+    var servermodel = graph.createNode("主机", 250, -50);
     servermodel.setStyle(Q.Styles.LABEL_POSITION, Q.Position.RIGHT_MIDDLE);
     servermodel.setStyle(Q.Styles.LABEL_OFFSET_X, 30);
     servermodel.image = "Q-server";
-    var clientmodel = graph.createNode("客户端", 300, 0);
+    var clientmodel = graph.createNode("客户端", 250, 0);
     clientmodel.setStyle(Q.Styles.LABEL_POSITION, Q.Position.RIGHT_MIDDLE);
     clientmodel.setStyle(Q.Styles.LABEL_OFFSET_X, 30);
 };
@@ -42,14 +51,14 @@ tenants = function () {
         success: function (result) {
             graph_init();
             console.info("Init Complete");
-            var mainNode = graph.createNode("主机", 0, -110);
+            var mainNode = graph.createNode("主机", -50, -110);
             mainNode.image = "Q-cloud";
             var len = result.length;
             console.info(result);
-            var cur_left = -200;
+            var cur_left = -250;
             var server = [];
             for (var k = 0; k < len; k++) {
-                server[k] = graph.createNode(result[k]['ServerName'], -50 + 100 * k, -10);
+                server[k] = graph.createNode(result[k]['ServerName'], -100 + 100 * k, -10);
                 server[k].image = "Q-server";
                 graph.createEdge(mainNode, server[k]);
                 var host = [];
@@ -59,7 +68,6 @@ tenants = function () {
                     graph.createEdge(server[k], host[i]);
                 }
             }
-            graph.moveToCenter(1);
         }
     })
 };
